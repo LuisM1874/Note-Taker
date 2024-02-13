0e4aa31424fad8b8f927aa14d2a99ad2001b5b83
Luis Moore <luismoore@Luiss-MacBook-Air.local>
2024-02-13 15:32:06 +0000

    server.js var

---
+++ server.js
@@ -0,0 +1,26 @@
+//Import Express.js, path package, contents of index.js
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// Get request to return index.html
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+//Get notes.html request when path ends in /notes
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+
